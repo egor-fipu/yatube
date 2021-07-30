71d9ebac775e932bb0f914594e19edd357e25b91
Егор Коростелев <egor.fipu@gmail.com>
2021-07-30 21:20:19 +0700

    correct

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l@jiyvsd+k*-x7t=blc300)+x2=z%)!-=wg1)gm*%fdh)%7f2&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
